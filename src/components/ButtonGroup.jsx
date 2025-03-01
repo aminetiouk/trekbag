0c93b786f78a7a5b895f0ce3fff10471ed3fa7d7
aminetiouk <tiouk.amine@gmail.com>
2025-03-01 22:57:50 +0000

    feat: create button group component

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,10 @@
+export default function ButtonGroup() {
+  return (
+    <section className="button-group">
+      <button className="btn">ButtonGroup</button>
+      <button className="btn">ButtonGroup</button>
+      <button className="btn">ButtonGroup</button>
+      <button className="btn">ButtonGroup</button>
+    </section>
+  )
+}
