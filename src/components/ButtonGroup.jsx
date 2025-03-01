c82fe8a3f36f9e1ed4e5d630a440db3329e068ef
aminetiouk <tiouk.amine@gmail.com>
2025-03-01 23:35:29 +0000

    feat: add button component

--- src/components/ButtonGroup.jsx
+++ src/components/ButtonGroup.jsx
@@ -1,10 +1,12 @@
+import Button from "./Button";
+
 export default function ButtonGroup() {
   return (
     <section className="button-group">
-      <button className="btn">ButtonGroup</button>
-      <button className="btn">ButtonGroup</button>
-      <button className="btn">ButtonGroup</button>
-      <button className="btn">ButtonGroup</button>
+      <Button type="secondary" text="Mark all as complete"/>
+      <Button type="secondary" text="Mark all as incomplete"/>
+      <Button type="secondary" text="Reset to initial"/>
+      <Button type="secondary" text="Remove all items"/>
     </section>
   )
 }
