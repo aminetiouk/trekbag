8ba3cfb0c68a885b5ba609c847a0e981c475f73f
aminetiouk <tiouk.amine@gmail.com>
2025-03-05 22:25:59 +0000

    feat: handle submit

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,10 +1,33 @@
+import { useState } from "react";
 import Button from "./Button";
 
-export default function AddItemForm() {
+export default function AddItemForm({ setItems }) {
+  const [itemText, setItemText] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!itemText) {
+      alert("Item can't be empty");
+      return;
+    }
+
+    const newItem = {
+      id: new Date().getTime(),
+      name: itemText,
+      packed: false
+    }
+
+    setItems(prev => [...prev, newItem])
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit} >
       <h2>Add an item</h2>
-      <input />
+      <input
+      value={itemText}
+      onChange={(e) => {
+        setItemText(e.target.value);
+      }} />
       <Button text="Add to list"/>
     </form>
   )
