31f7ac4ed2a30a213ffd2734e8085950813742fb
aminetiouk <tiouk.amine@gmail.com>
2025-03-09 18:07:06 +0000

    feat: add Remove all items function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,29 @@
-import { useState } from "react"
-import BackgroundHeading from "./BackgroundHeading"
-import Footer from "./Footer"
-import Header from "./Header"
-import ItemList from "./ItemList"
-import Sidebar from "./Sidebar"
-import { initialItems } from "../lib/constants"
+import { useState } from 'react';
+import BackgroundHeading from './BackgroundHeading';
+import Footer from './Footer';
+import Header from './Header';
+import ItemList from './ItemList';
+import Sidebar from './Sidebar';
+import { initialItems } from '../lib/constants';
 
 function App() {
-  const [items, setItems] = useState(initialItems)
+  const [items, setItems] = useState(initialItems);
 
-  const handleAddItem = (newItemText) => {
+  const handleAddItem = newItemText => {
     const newItem = {
       id: new Date().getTime(),
       name: newItemText,
       packed: false
-    }
+    };
 
     const newItems = [...items, newItem];
     setItems(newItems);
-  }
+  };
+
+  const handleRemoveAllItems = () => {
+    setItems([]);
+  };
+
 
   return (
     <>
@@ -26,12 +31,15 @@ function App() {
 
       <main>
         <Header />
-        <ItemList items={items}/>
-        <Sidebar handleAddItem={handleAddItem} />
+        <ItemList items={items} />
+        <Sidebar
+          handleAddItem={handleAddItem}
+          handleRemoveAllItems={handleRemoveAllItems}
+        />
       </main>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
