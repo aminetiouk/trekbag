75e5dbc73154b33bbb19e2e770ad380cb7cb95aa
aminetiouk <tiouk.amine@gmail.com>
2025-03-10 21:42:32 +0000

    feat: implement Delete Item function

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,20 +1,33 @@
-export default function ItemList({ items }) {
+export default function ItemList({
+  items,
+  handleDeleteItem,
+}) {
   return (
     <ul>
-      {items.map((item) => {
-        return <Item key={item.id} item={item} />;
+      {items.map(item => {
+        return (
+          <Item
+            key={item.id}
+            item={item}
+            handleDeleteItem={handleDeleteItem}
+          />
+        );
       })}
     </ul>
-  )
+  );
 }
 
-function Item({ item }) {
+function Item({ item, handleDeleteItem }) {
   return (
     <li className="item">
       <label>
-        <input type="checkbox" checked={item.packed} />
-        {item.name}</label>
-        <button>❌</button>
+        <input
+          type="checkbox"
+          checked={item.packed}
+        />
+        {item.name}
+      </label>
+      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
     </li>
-  )
+  );
 }
