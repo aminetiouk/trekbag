c029c8e889cdb29b36d1cd5a5eb4cd454475cc34
aminetiouk <tiouk.amine@gmail.com>
2025-03-11 23:56:51 +0000

    feat: implement prop NumberOfItemsPacked

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,7 +1,7 @@
-export default function Counter({ totalNumberOfItems }) {
+export default function Counter({ totalNumberOfItems, NumberOfItemsPacked }) {
   return (
     <p>
-      <b>1</b> / {totalNumberOfItems} items packed
+      <b>{NumberOfItemsPacked}</b> / {totalNumberOfItems} items packed
     </p>
   );
 }
