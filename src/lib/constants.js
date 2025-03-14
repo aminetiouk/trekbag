f1581ea3476dcd4eb501de2e4701a532373386ad
aminetiouk <tiouk.amine@gmail.com>
2025-03-14 18:05:56 +0000

    feat: move sortingOption and secondaryButtons

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -15,3 +15,37 @@ export const initialItems = [
     packed: false
   }
 ]
+
+export const sortingOption = [
+  {
+    label: 'Sort by default',
+    value: 'default'
+  },
+  {
+    label: 'Sort by packed',
+    value: 'packed'
+  },
+  {
+    label: 'Sort by unpacked',
+    value: 'unpacked'
+  }
+];
+
+export const secondaryButtons = (handlers) => [
+  {
+    text: 'Mark all as complete',
+    onClick: handlers.handleMarkAllAsComplete
+  },
+  {
+    text: 'Mark all as incomplete',
+    onClick: handlers.handleMarkAllAsIncomplete
+  },
+  {
+    text: 'Reset to initial',
+    onClick: handlers.handleResetToInitial
+  },
+  {
+    text: 'Remove all items',
+    onClick: handlers.handleRemoveAllItems
+  }
+];
