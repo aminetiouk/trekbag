468091b65ec5628cd2b5ed944fafd4e04ee8a618
aminetiouk <tiouk.amine@gmail.com>
2025-03-14 17:55:59 +0000

    feat: wrap components with context provider

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,90 +1,21 @@
-import { useState } from 'react';
 import BackgroundHeading from './BackgroundHeading';
 import Footer from './Footer';
 import Header from './Header';
 import ItemList from './ItemList';
 import Sidebar from './Sidebar';
-import { initialItems } from '../lib/constants';
-import { useEffect } from 'react';
+import ItemContextProvider from '../context/itemContextProvider';
 
 function App() {
-  const [items, setItems] = useState(
-    () => JSON.parse(localStorage.getItem('items')) || initialItems
-  );
-
-  const handleAddItem = newItemText => {
-    const newItem = {
-      id: new Date().getTime(),
-      name: newItemText,
-      packed: false
-    };
-
-    const newItems = [...items, newItem];
-    setItems(newItems);
-  };
-
-  const handleDeleteItem = id => {
-    const newItems = items.filter(item => item.id !== id);
-    setItems(newItems);
-  };
-
-  const handleToggleItem = id => {
-    const newItems = items.map(item => {
-      if (item.id === id) {
-        return { ...item, packed: !item.packed };
-      }
-      return item;
-    });
-    setItems(newItems);
-  };
-
-  const handleRemoveAllItems = () => {
-    setItems([]);
-  };
-
-  const handleResetToInitial = () => {
-    setItems(initialItems);
-  };
-
-  const handleMarkAllAsComplete = () => {
-    const newItems = items.map(item => {
-      return { ...item, packed: true };
-    });
-    setItems(newItems);
-  };
-
-  const handleMarkAllAsIncomplete = () => {
-    const newItems = items.map(item => {
-      return { ...item, packed: false };
-    });
-    setItems(newItems);
-  };
-
-  useEffect(() => {
-    localStorage.setItem('items', JSON.stringify(items));
-  }, [items]);
-
   return (
     <>
       <BackgroundHeading />
 
       <main>
-        <Header
-          totalNumberOfItems={items.length}
-          NumberOfItemsPacked={items.filter(item => item.packed).length}
-        />
-        <ItemList
-          items={items}
-          handleDeleteItem={handleDeleteItem}
-          handleToggleItem={handleToggleItem}
-        />
-        <Sidebar
-          handleAddItem={handleAddItem}
-          handleRemoveAllItems={handleRemoveAllItems}
-          handleResetToInitial={handleResetToInitial}
-          handleMarkAllAsComplete={handleMarkAllAsComplete}
-          handleMarkAllAsIncomplete={handleMarkAllAsIncomplete}
-        />
+        <ItemContextProvider>
+          <Header />
+          <ItemList />
+          <Sidebar />
+        </ItemContextProvider>
       </main>
       <Footer />
     </>
