721b5eba222d5ea392484f1623f0dc2a84e3b332
aminetiouk <tiouk.amine@gmail.com>
2025-03-23 15:40:53 +0000

    Chore: remove old JSX components after migration to TypeScript

--- src/components/Counter.jsx
+++
@@ -1,7 +0,0 @@
-export default function Counter({ totalNumberOfItems, NumberOfItemsPacked }) {
-  return (
-    <p>
-      <b>{NumberOfItemsPacked}</b> / {totalNumberOfItems} items packed
-    </p>
-  );
-}
